e3be4c8301484b669cabc6b318c5a157550d03ee
debanjan611 <68823461+debanjan611@users.noreply.github.com>
2021-10-02 21:31:36 +0530

    Add files via upload

---
+++ Python/Reverse the array.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 20 10:44:27 2021
+
+@author: Debanjan Rudra
+"""
+
+if __name__=='__main__':
+    
+    for _ in range(int(input())):
+        
+        s = input()
+        
+        print(s[::-1])
