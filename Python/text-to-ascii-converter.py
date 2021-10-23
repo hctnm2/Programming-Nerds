a852253618b57432a3c2db87ec58304c2a3cfa86
MD UDOY HASAN <udoyudoy111@gmail.com>
2021-10-24 00:48:13 +0600

    Revert "Created a text to ASCII generator"

--- Python/text-to-ascii-converter.py
+++
@@ -1,5 +0,0 @@
-from termcolor import cprint
-from pyfiglet import figlet_format
-
-data = input("Please enter the text that you want to convert: ")
-cprint(figlet_format(data, font="slant"), attrs=["bold"])
